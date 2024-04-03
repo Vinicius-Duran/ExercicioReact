596bc8b3788560687528708ca7d9200a4bd949b0
Viniciusmeta <metaemarketing2@gmail.com>
2024-04-03 19:14:46 -0300

    imagem

--- src/Componentes/Main/SubComponentes/MostrarTela/MostrarTela.jsx
+++ src/Componentes/Main/SubComponentes/MostrarTela/MostrarTela.jsx
@@ -1,17 +1,40 @@
-import "./MostrarTela.css";
 import React from "react";
+import "./MostrarTela.css";
+import ImagemTela1 from './Lula.jpg'; // Importe a imagem para a tela 1
+import ImagemTela2 from './Bob.jpg'; // Importe a imagem para a tela 2
+import ImagemTela3 from './Patrick.jpg'; // Importe a imagem para a tela 3
 
 const MostrarTela = () => {
   const [tela, setTela] = React.useState("");
+  const [imagem, setImagem] = React.useState(null);
 
   const mostrarTexto = (texto) => {
     setTela(texto);
+    definirImagem(texto); // Atualiza a imagem ao mudar de tela
+  };
+
+  const definirImagem = (texto) => {
+    // Define a imagem com base no texto da tela
+    switch (texto) {
+      case "Tela I":
+        setImagem(ImagemTela1);
+        break;
+      case "Tela II":
+        setImagem(ImagemTela2);
+        break;
+      case "Tela III":
+        setImagem(ImagemTela3);
+        break;
+      default:
+        setImagem(null);
+    }
   };
 
   return (
     <div className="mostrar">
+      {imagem && <img src={imagem} alt="Imagem da tela" />} {/* Exibe a imagem se estiver definida */}
       <button onClick={() => mostrarTexto("Tela I")} className="butao">
-        Tela I
+        Tela I 
       </button>
       <button onClick={() => mostrarTexto("Tela II")} className="butao">
         Tela II
